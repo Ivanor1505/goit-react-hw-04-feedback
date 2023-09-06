7a257b48dfb0d49a895780346c3aec1300f6cf4d
Ivanor1505 <ivanor1505@gmail.com>
2023-09-06 11:17:16 +0300

    React HW4.1 to case

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,52 +3,53 @@ import { Feedback } from './FeedbackOptons/FeedbackOpt';
 import { Statistics } from './Statistics/Statistics';
 import { Section } from './Section/Section';
 
-
-
 export const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-  
-  
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleFeedback = type => {
-    setFeedback(prevState => ({...prevState, [type]: prevState[type] + 1 }));
+    switch (type) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
   const countTotalFeedback = () => {
-    const { good, neutral, bad } = feedback;
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercent = () => {
-    const { good } = feedback;
     const total = countTotalFeedback();
     return total ? Math.round((good / total) * 100) : 0;
   };
 
-  const { good, neutral, bad } = feedback;
-
-      return (
-      <div>
-        <Section title="Please leave feedback">
-          <Feedback
-            options={Object.keys(feedback)}
-            onFeedback={handleFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={countTotalFeedback()}
-            positivePercentage={countPositiveFeedbackPercent()}
-          />
-        </Section>
-      </div>
-    );
-  }
-
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <Feedback
+          options={['good', 'neutral', 'bad']}
+          onFeedback={handleFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercent()}
+        />
+      </Section>
+    </div>
+  );
+};
